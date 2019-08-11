1e105564f7ec9b5a5de13bdbb6d829292d18dc7c
johnwanjema <jonwanjema@gmail.com>
2019-08-11 15:34:10 +0300

    added dropdown for profile and edit profile

--- myneighbour/urls.py
+++ myneighbour/urls.py
@@ -5,8 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.welcome,name='welcome'),
-    url('profile/', views.profile, name='profile'), 
-    url(r'^profile/edit$',views.update_profile,name='edit'),   
+    url(r'^profile/edit$',views.update_profile,name='edit'),
+    url('profile/', views.profile, name='profile'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
